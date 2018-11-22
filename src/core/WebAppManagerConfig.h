6326d7ccbdda615341488d8b6d622d7771558cc1
Nick Diego Yamane <nickdiego@igalia.com>
2018-11-22 17:17:14 -0400

    [agl][qtless] Do not use QString in WebAppManagerConfig
    [SPEC-1911] Qt-less WAM: replace qt basic data structures
    
    Signed-off-by: Nick Diego Yamane <nickdiego@igalia.com>

--- src/core/WebAppManagerConfig.h
+++ src/core/WebAppManagerConfig.h
@@ -20,7 +20,6 @@
 #include <map>
 #include <string>
 
-#include <QString>
 #include <QVariant>
 
 class WebAppManagerConfig {
@@ -28,50 +27,50 @@ public:
     WebAppManagerConfig();
     virtual ~WebAppManagerConfig() {}
 
-    virtual QString getWebAppFactoryPluginTypes() const { return m_webAppFactoryPluginTypes; }
-    virtual QString getWebAppFactoryPluginPath() const { return m_webAppFactoryPluginPath; }
+    virtual std::string getWebAppFactoryPluginTypes() const { return m_webAppFactoryPluginTypes; }
+    virtual std::string getWebAppFactoryPluginPath() const { return m_webAppFactoryPluginPath; }
     virtual int getSuspendDelayTime() const { return m_suspendDelayTime; }
     virtual int getMaxCustomSuspendDelayTime() const { return m_maxCustomSuspendDelayTime; }
-    virtual QString getWebProcessConfigPath() const { return m_webProcessConfigPath; }
+    virtual std::string getWebProcessConfigPath() const { return m_webProcessConfigPath; }
     virtual bool isInspectorEnabled() const { return m_inspectorEnabled; }
     virtual bool isDevModeEnabled() const { return m_devModeEnabled; }
-    virtual QString getErrorPageUrl() const { return m_errorPageUrl; }
+    virtual std::string getErrorPageUrl() const { return m_errorPageUrl; }
     virtual std::string getTelluriumNubPath() const { return m_telluriumNubPath; }
     virtual void postInitConfiguration();
     virtual bool isDynamicPluggableLoadEnabled() const { return m_dynamicPluggableLoadEnabled; }
     virtual bool isPostWebProcessCreatedDisabled() const { return m_postWebProcessCreatedDisabled; }
     virtual bool isCheckLaunchTimeEnabled() const { return m_checkLaunchTimeEnabled; }
     virtual bool isUseSystemAppOptimization() const { return m_useSystemAppOptimization; }
-    virtual QString getUserScriptPath() const { return m_userScriptPath; }
+    virtual std::string getUserScriptPath() const { return m_userScriptPath; }
     virtual std::string getName() const { return m_name; }
 
     virtual bool isLaunchOptimizationEnabled() const { return m_launchOptimizationEnabled; }
 
 protected:
-    virtual QVariant getConfiguration(QString name);
-    virtual void setConfiguration(QString name, QVariant value);
+    virtual QVariant getConfiguration(std::string name);
+    virtual void setConfiguration(const std::string& name, QVariant value);
 
 private:
     void initConfiguration();
 
-    QString m_webAppFactoryPluginTypes;
-    QString m_webAppFactoryPluginPath;
+    std::string m_webAppFactoryPluginTypes;
+    std::string m_webAppFactoryPluginPath;
     int m_suspendDelayTime;
     int m_maxCustomSuspendDelayTime;
-    QString m_webProcessConfigPath;
+    std::string m_webProcessConfigPath;
     bool m_devModeEnabled;
     bool m_inspectorEnabled;
-    QString m_errorPageUrl;
+    std::string m_errorPageUrl;
     std::string m_telluriumNubPath;
     bool m_dynamicPluggableLoadEnabled;
     bool m_postWebProcessCreatedDisabled;
     bool m_checkLaunchTimeEnabled;
     bool m_useSystemAppOptimization;
     bool m_launchOptimizationEnabled;
-    QString m_userScriptPath;
+    std::string m_userScriptPath;
     std::string m_name;
 
-    std::map<QString, QVariant> m_configuration;
+    std::map<std::string, QVariant> m_configuration;
 };
 
 #endif /* WEBAPPMANAGERCONFIG_H */
