96fce9cc3abf0fad57dd3e203e46690f80518194
Marius Vlad <marius.vlad@collabora.com>
2020-03-22 21:23:19 +0200

    agl/WebAppManagerServiceAGL: Allow to pass the surface role and the panel type
    Signed-off-by: Marius Vlad <marius.vlad@collabora.com>

--- src/agl/WebAppManagerServiceAGL.h
+++ src/agl/WebAppManagerServiceAGL.h
@@ -24,7 +24,8 @@ public:
     bool isHostServiceRunning();
 
     void setStartupApplication(const std::string& startup_app_id,
-        const std::string& startup_app_uri, int startup_app_surface_id);
+        const std::string& startup_app_uri, int startup_app_surface_id,
+	int _surface_role, int _panel_type);
     void setAppIdForEventTarget(const std::string& app_id);
 
     void launchOnHost(int argc, const char **argv);
@@ -62,6 +63,9 @@ private:
 
     std::string startup_app_id_;
     std::string startup_app_uri_;
+    int surface_role;
+    int panel_type;
+
     int startup_app_surface_id_;
     OneShotTimer<WebAppManagerServiceAGL> startup_app_timer_;
 
